2b279b03aaa3cd4e39ada48acb2df51a240c73ec
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-03-23 01:04:06 +0700

    signIn

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const { default: mongoose } = require("mongoose");
+const routes = require('./routes');
+const bodyParser = require("body-parser");
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3002
+
+app.use(bodyParser.json())
+
+routes(app)
+
+// app.get('/', (req, res) => {
+//     return res.send('Hello world! Everyone')
+// })
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+.then(() => {
+    console.log('Connect db success!');
+})
+.catch((err) => {
+    console.log(err);
+})
+
+app.listen(port, () => {
+    console.log('Server is running in port: ', + port);
+})
+
+
+
