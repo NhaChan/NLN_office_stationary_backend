eb98ee4eadeadfa4e33259231dcc479264bf91c7
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-03-28 10:43:15 +0700

    CRUD product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
 const UserRouter = require('./UserRouter')
-// const ProductRouter = require('./ProductRouter')
-// const OrderRouter = require('./OrderRouter')
-// const PaymentRouter = require('./PaymentRouter')
+const ProductRouter = require('./ProductRouter')
 
 const routes = (app) => {
     app.use('/api/user', UserRouter)
-    
-    // app.use('/api/product', ProductRouter)
-    // app.use('/api/order', OrderRouter)
-    // app.use('/api/payment', PaymentRouter)
+    app.use('/api/product', ProductRouter)
 }
 
 module.exports = routes
