2b4f11bcd80589cab66493494f151252a927c72d
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-03-31 14:25:42 +0700

    signup user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,33 +3,36 @@ const JwtService = require('../services/JwtService')
 
 const createUser = async (req, res) => {
     try {
-        const { name, email, password, confirmPassword, phone } = req.body
-        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-        const isCheckEmail = reg.test(email)
+        const { name, email, password, confirmPassword, phone } = req.body;
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        const isCheckEmail = reg.test(email);
+
         if (!email || !password || !confirmPassword) {
-            return res.status(200).json({
+            return res.status(400).json({
                 status: 'ERR',
                 message: 'The input is required'
-            })
+            });
         } else if (!isCheckEmail) {
-            return res.status(200).json({
+            return res.status(400).json({
                 status: 'ERR',
-                message: 'The input is email'
-            })
+                message: 'Invalid email format'
+            });
         } else if (password !== confirmPassword) {
-            return res.status(200).json({
+            return res.status(400).json({
                 status: 'ERR',
-                message: 'The password is equal confirmPassword'
-            })
+                message: 'The password does not match the confirmed password'
+            });
         }
-        const response = await UserService.createUser(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+
+        const response = await UserService.createUser(req.body);
+        return res.status(201).json(response);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
     }
-}
+};
+
 
 const loginUser = async (req, res) => {
     try {
